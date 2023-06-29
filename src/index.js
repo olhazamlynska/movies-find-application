639fee31b7d59d541f3e56e4fa042c2038894bf5
olhazamlynska <olya.sonechko@gmail.com>
2023-06-29 19:32:23 +0300

    change name

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/movies-find-application">
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
